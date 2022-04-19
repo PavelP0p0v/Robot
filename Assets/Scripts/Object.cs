42d4b86826a772b8d12a6333fd7deb657712cbd7
PavelP0p0v <103581064+PavelP0p0v@users.noreply.github.com>
2022-04-19 17:15:23 +0300

    123
    12

--- Assets/Scripts/Object.cs
+++ Assets/Scripts/Object.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Object : MonoBehaviour
 {
     // Start is called before the first frame update
-    private float Fall = -30f;
+    private float Fall = -100f;
 
     private void OnCollisionEnter(Collision collision)
     {
